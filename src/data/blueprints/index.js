d507a1e930a8844a22c1c57baef07065d7c6932c
311303jf <311303@dadeschools.net>
2026-05-29 22:55:01 -0400

    Update index.js

--- src/data/blueprints/index.js
+++ src/data/blueprints/index.js
@@ -1,1 +1,22 @@
+import { UNIT_1_BLUEPRINT } from "./unit1.js";
+import { UNIT_2_BLUEPRINT } from "./unit2.js";
+import { UNIT_3_BLUEPRINT } from "./unit3.js";
+import { UNIT_4_BLUEPRINT } from "./unit4.js";
+import { UNIT_5_BLUEPRINT } from "./unit5.js";
 
+export const CURRICULUM_BLUEPRINTS = {
+  course: {
+    id: "algebra-1",
+    title: "Algebra 1",
+    state: "Florida BEST",
+    platform: "Algebra OS",
+    version: "2.0.0"
+  },
+  units: [
+    UNIT_1_BLUEPRINT,
+    UNIT_2_BLUEPRINT,
+    UNIT_3_BLUEPRINT,
+    UNIT_4_BLUEPRINT,
+    UNIT_5_BLUEPRINT
+  ]
+};
