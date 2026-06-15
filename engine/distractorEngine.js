d1b4c9c9b531c8bf3290823a3423132b880cbc40
311303jf <311303@dadeschools.net>
2026-06-14 21:42:25 -0400

    Update distractorEngine.js

--- engine/distractorEngine.js
+++ engine/distractorEngine.js
@@ -277,18 +277,18 @@ function gcd(a, b) {
 function prettifyExponent(text) {
 
   const superscripts = {
-    "0": "⁰",
-    "1": "¹",
-    "2": "²",
-    "3": "³",
-    "4": "⁴",
-    "5": "⁵",
-    "6": "⁶",
-    "7": "⁷",
-    "8": "⁸",
-    "9": "⁹",
-    "-": "⁻"
-  };
+  "0": "⁰",
+  "1": "¹",
+  "2": "²",
+  "3": "³",
+  "4": "⁴",
+  "5": "⁵",
+  "6": "⁶",
+  "7": "⁷",
+  "8": "⁸",
+  "9": "⁹",
+  "-": "⁻"
+};
 
   return String(text).replace(/\^(-?\d+)/g, function(match, exponent) {
     return String(exponent)
